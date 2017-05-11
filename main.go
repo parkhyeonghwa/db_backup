fdb4b336ba672357d318ad38d6ccd43a54b0e6fe
Kieran McEniff <kieran.mceniff@thedrum.com>
2017-05-11 16:53:47 +0100

    adding user input to select which database from a list they require. Added logic to determine which file is needed based on user input and grab that file from the server

--- main.go
+++ main.go
@@ -7,9 +7,50 @@ import(
 	"log"
 	"github.com/pkg/sftp"
 	"io/ioutil"
+	"time"
+	"bufio"
+	"strings"
 )
 
 func main(){
+
+	var srcPath string
+	var dstPath = "/home/carnyx/Desktop/"
+	var filename string
+
+	fmt.Println("Please select which database you would like to import:\n" +
+		"0 - Old awards (chipshop)\n" +
+		"1 - Jobs\n" +
+		"2 - Awards Multisite\n" +
+		"3 - RAR\n" +
+		"4 - The Drum\n")
+
+	reader := bufio.NewReader(os.Stdin)
+	c, _ := reader.ReadString('\n')
+	c = strings.TrimSpace(c)
+
+	yDate := time.Now().AddDate(0, 0, -1)
+
+	switch c {
+		case "0":
+			srcPath = "/backup/aws-arctic-fox/"
+			filename = "all_arctic_fox_databases-" + yDate.Format("20060102") + ".sql.gz"
+		case "1":
+			srcPath = "/backup/aws-londinium/"
+			filename = "jobs-" + yDate.Format("20060102") + ".sql.gz"
+		case "2":
+			srcPath = "/backup/aws-londinium/"
+			filename = "awards_prod-" + yDate.Format("20060102") + ".sql.gz"
+		case "3":
+			srcPath = "/backup/aws-robocod-db/"
+			filename = yDate.Format("2006_01_02") + "_recommended_agencies.sql.gz"
+		case "4":
+			srcPath = "/backup/aws-yingyang/"
+			filename = "thedrum_prod-" + yDate.Format("20060102") + ".sql.gz"
+		default:
+			panic("You didn't choose something from the list")
+	}
+
 
 	key, err := ioutil.ReadFile("/home/carnyx/.ssh/id_rsa")
 	if err != nil {
@@ -42,10 +83,6 @@ func main(){
 		log.Fatal(err)
 	}
 	defer sftp.Close()
-
-	srcPath := "/backup/aws-londinium/"
-	dstPath := "/home/carnyx/Desktop/"
-	filename := "awards_prod-20170510.sql.gz"
 
 	// Open the source file
 	srcFile, err := sftp.Open(srcPath + filename)
